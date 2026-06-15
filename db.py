34f250048774551f304cdb671e4b2dced41fd3cf
CP-byte <parabchandan1234@gmail.com>
2026-06-15 12:06:11 +0530

    changes in db.py

---
+++ db.py
@@ -0,0 +1,22 @@
+import os
+import pyodbc
+
+def get_connection():
+
+    server = os.getenv("SQL_SERVER")
+    database = os.getenv("SQL_DATABASE")
+    username = os.getenv("SQL_USER")
+    password = os.getenv("SQL_PASSWORD")
+
+    conn_str = (
+        "DRIVER={ODBC Driver 18 for SQL Server};"
+        f"SERVER={server};"
+        f"DATABASE={database};"
+        f"UID={username};"
+        f"PWD={password};"
+        "Encrypt=yes;"
+        "TrustServerCertificate=no;"
+        "Connection Timeout=30;"
+    )
+
+    return pyodbc.connect(conn_str)
